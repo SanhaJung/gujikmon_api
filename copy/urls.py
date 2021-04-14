ebe2e89e154e5d793565dd4e4529d8aa80422403
kssgit <tjdtnmail@gmail.com>
2021-04-14 17:37:44 +0900

    [UPDATE] view.py change json_list

--- copy/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns =[
-    path('companies/',views.companies),
-    path('company/search/<str:keyword>',views.searchCompany),
-]
--- gujikmon_api/api_server/views.py
+++ gujikmon_api/api_server/views.py
@@ -3,6 +3,7 @@ from .coSerializer import CoSerializer
 from .filterservices import coFiltering
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 import json
 # from django.http import JsonResponse
 
